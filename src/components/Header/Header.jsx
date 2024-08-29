b7e82faae83b3f3fe18d4f40fbfec6eb970fcfc8
Natalie Gillam <ngillam@hotmail.co.uk>
2024-08-29 22:32:57 +0100

    added header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header id="header">
+        <img src="investment-calculator-logo.png" alt="" />
+    <h1>Investment Calculator</h1>
+    </header>
   )
 }
 
